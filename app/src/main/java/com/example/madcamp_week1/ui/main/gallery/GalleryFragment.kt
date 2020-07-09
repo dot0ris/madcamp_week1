27abf58bc669decb0862ce91e47fbe216e653938
Seongjae Im <seongjae114@gmail.com>
2020-07-09 21:51:14 +0900

    improved loading speed of drawable images

--- app/src/main/java/com/example/madcamp_week1/ui/main/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/madcamp_week1/ui/main/gallery/GalleryFragment.kt
@@ -1,25 +1,60 @@
 package com.example.madcamp_week1.ui.main.gallery
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madcamp_week1.R
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
+
 
 class GalleryFragment : Fragment() {
     private lateinit var recyclerView : RecyclerView
     private lateinit var adapter : GalleryViewAdapter
     private var list = mutableListOf<Int>()
+    private lateinit var listDir : List<String>
+    private var cnt = 1
+
+    /*
+    private fun saveImage(drawableId : Int) : String {
+        val bitmap = BitmapFactory.decodeResource(resources, drawableId)
+        var file = context?.getDir("images", Context.MODE_PRIVATE)
+        file = File(file, "image${cnt++}.jpg")
+        try{
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        }catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return file.toString()
+    }
+    */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        list = mutableListOf<Int>(R.drawable.image01, R.drawable.image02, R.drawable.image03)
-
+        var file = context?.getDir("images", Context.MODE_PRIVATE)
+        for (i in 1..22){
+            val url = "drawable/image"+i.toString().padStart(2, '0')
+            list.add(resources.getIdentifier(url, "drawable", activity!!.packageName))
+        }
+        //listDir = list.map{saveImage(it)}
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -27,9 +62,9 @@ class GalleryFragment : Fragment() {
         val recyclerView : RecyclerView = view.findViewById(R.id.recycler_view)
         recyclerView.apply{
             layoutManager = GridLayoutManager(activity, 2)
-            adapter =
-                GalleryViewAdapter(list)
+            adapter = GalleryViewAdapter(context, list)
         }
         return view
     }
+
 }
--- app/src/main/java/com/example/madcamp_week1/ui/main/gallery/GalleryViewAdapter.kt
+++ app/src/main/java/com/example/madcamp_week1/ui/main/gallery/GalleryViewAdapter.kt
@@ -1,21 +1,29 @@
 package com.example.madcamp_week1.ui.main.gallery
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.madcamp_week1.R
+import kotlin.coroutines.coroutineContext
 
-class GalleryViewAdapter(private val imageList: List<Int>)
+class GalleryViewAdapter(private val context : Context, private val imageList: List<Int>)
     : RecyclerView.Adapter<GalleryViewAdapter.Holder>() {
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val imageView : ImageView = itemView.findViewById<ImageView>(R.id.id_image)
 
-        fun bind(img : Int){
-            imageView.setImageResource(img)
+        fun bind(img_path : Int){
+            Glide.with(context)
+                .load(img_path)
+                .override(400, 300)
+                .into(imageView)
         }
     }
 
