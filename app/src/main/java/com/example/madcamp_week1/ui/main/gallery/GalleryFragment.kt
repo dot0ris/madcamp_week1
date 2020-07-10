2c7da995e79409e6a64740301158bedb05cb58f4
Seongjae Im <seongjae114@gmail.com>
2020-07-10 19:36:41 +0900

    Added column number adjusting menu

--- app/src/main/java/com/example/madcamp_week1/ui/main/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/madcamp_week1/ui/main/gallery/GalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.madcamp_week1.ui.main.gallery
 
+import android.app.Dialog
 import android.content.Context
 import android.content.ContextWrapper
 import android.graphics.Bitmap
@@ -10,6 +11,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.AdapterView
+import android.widget.Button
+import android.widget.NumberPicker
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -23,7 +26,7 @@ import java.io.OutputStream
 import java.util.*
 
 
-class GalleryFragment : Fragment(){
+class GalleryFragment : Fragment(), NumberPicker.OnValueChangeListener{
     private lateinit var recyclerView : RecyclerView
     private lateinit var adapter : GalleryViewAdapter
     private lateinit var img_paths : MutableList<String>
@@ -71,12 +74,30 @@ class GalleryFragment : Fragment(){
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.action_gallery1 -> Snackbar.make(view!!, "Gallery Option 1", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            R.id.action_gallery1 -> showColumnOption()
             R.id.action_gallery2 -> Snackbar.make(view!!, "Gallery Option 2", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun showColumnOption(){
+        val d = Dialog(context!!)
+        d.setTitle("NumberPicker")
+        d.setContentView(R.layout.dialog)
+        val b1 = d.findViewById<Button>(R.id.button1)
+        val np = d.findViewById<NumberPicker>(R.id.numberPicker1)
+        np.maxValue = 5; np.minValue = 1
+        np.wrapSelectorWheel = false
+        np.setOnValueChangedListener(this)
+        b1.setOnClickListener(View.OnClickListener {
+            val recyclerView = view!!.findViewById<RecyclerView>(R.id.recycler_view)
+            recyclerView.layoutManager = GridLayoutManager(activity, np.value)
+            d.dismiss()
+        })
+        d.show()
+    }
+
+    override fun onValueChange(p0: NumberPicker?, p1: Int, p2: Int) {}
 
 }
